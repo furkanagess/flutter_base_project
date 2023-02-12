17b35a963cae6f1f017629c178b42ede8002601c
furkanagess <caglarrfurkann@gmail.com>
2023-02-13 00:25:15 +0300

    widgets

---
+++ lib/core/widgets/avatar/onboard_circle_avatar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:base_project/core/extension/context_extension.dart';
+
+class OnBoardCircle extends StatelessWidget {
+  final bool isSelected;
+  const OnBoardCircle({super.key, required this.isSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: context.paddingLow,
+      child: CircleAvatar(
+        backgroundColor: context.colors.onPrimary.withOpacity(isSelected ? 1 : 0.2),
+        radius: isSelected ? 8 : 5,
+      ),
+    );
+  }
+}
